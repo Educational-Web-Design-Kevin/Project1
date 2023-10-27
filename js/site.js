37f291a149079292c084abc321fb174294731b2d
Kevin-Alexis-Aguilar <89360177+Kevin-Alexis-Aguilar@users.noreply.github.com>
2023-10-27 11:15:39 -0600

    Add js code to validate email field

--- js/site.js
+++ js/site.js
@@ -2,3 +2,8 @@ jQuery(function($) {
     $('html').removeClass('nojs');
     $('html').addClass('hasjs');
 });
+
+function isEmail(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email);
+  }
